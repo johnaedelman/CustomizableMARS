fd481f9baeece862940bcb0d77bcc93feb158cef
johnaedelman <johnaedelman@gmail.com>
2025-11-10 00:47:36 -0800

    Updated CustomLang builder

--- BuildCustomLang.java
+++ BuildCustomLang.java
@@ -1,7 +1,10 @@
-package CustomLangJarBuilder;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.*;
 
 public class BuildCustomLang{
     private final static String FOLDER_PATH = "mars/mips/instructions/customlangs/";
@@ -25,6 +28,20 @@ public class BuildCustomLang{
         }
         folder.delete();
     }
+
+    private static void runJavaCommand(String[] cmd) throws IOException, InterruptedException{
+        ProcessBuilder pb = new ProcessBuilder(cmd);
+        String outputLine;
+        pb.redirectErrorStream(true);
+        Process currentProcess = pb.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(currentProcess.getInputStream()));
+        while ((outputLine = reader.readLine()) != null)
+            System.out.println(outputLine);
+        currentProcess.waitFor();
+        if (currentProcess.exitValue() != 0)
+            System.exit(-1);
+        return;
+    }
     public static void main(String[] args) throws IOException, InterruptedException{
         String langFile = "";
         if (args.length == 1){
@@ -39,23 +56,21 @@ public class BuildCustomLang{
             System.out.println("File not found in " + FOLDER_PATH + "! Input only the name of your custom language file, i.e. \"ExampleCustomAssembly.java\".");
             System.exit(-1);
         }
+        String[] ensure_mars_compliance_command = {"javac", "Mars.java"};
         String[] compile_command = {"javac", "-d", OUT_PATH, FOLDER_PATH + langFile + ".java"};
         String[] build_jar_command = {"jar", "cf", langFile + ".jar", "-C", OUT_PATH, "."};
-        
-        ProcessBuilder pb = new ProcessBuilder(compile_command);
-        Process currentProcess = pb.start();
-        currentProcess.waitFor();
-        pb = new ProcessBuilder(build_jar_command);
-        currentProcess = pb.start();
-        currentProcess.waitFor();
+
+        runJavaCommand(ensure_mars_compliance_command);
+        System.out.println("[PROCESS] MARS executable is up to date.");
+        runJavaCommand(compile_command);
+        System.out.println("[PROCESS] " + langFile + ".java file compiled successfully.");
+        runJavaCommand(build_jar_command);
+        System.out.println("[PROCESS] " + langFile + ".jar file built successfully.");
         File outDir = new File(OUT_PATH);
         deleteDirectory(outDir);
-        File jarFile = new File(langFile + ".jar");
-        File jarFilePath = new File(FOLDER_PATH + langFile + ".jar");
-        if (jarFilePath.isFile()){
-            jarFilePath.delete();
-        }
-        jarFile.renameTo(new File(FOLDER_PATH + langFile + ".jar"));
-        System.out.println("JAR built successfully! Open MARS LE and check it out.");
+        System.out.println("[PROCESS] Cleanup successful.");
+        Files.move(Paths.get(langFile + ".jar"), Paths.get(FOLDER_PATH + langFile + ".jar"), StandardCopyOption.REPLACE_EXISTING);
+        System.out.println("[PROCESS] JAR file successfully moved to " + FOLDER_PATH + langFile + ".jar.");
+        System.out.println("[SUCCESS] JAR built successfully! Open MARS LE and check it out.");
     }
 }
--- mips1.asm
+++ mips1.asm
@@ -1,2 +1,10 @@
-li $t0, 1
-addi $t0, $t0, 2
+.data
+message: .asciiz "Power level drained by 500!!!\n"
+.text
+pow $t1, 8001
+pow $t2, 4
+kai $t1, $t2
+loop:
+pow $t1, -500
+tp message
+it $t1, loop
